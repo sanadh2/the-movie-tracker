264ae4e736bec22f5bdb7c392909bd7ebfc80bcc
sanadh <sanadhakku@gmail.com>
2024-08-21 08:28:56 +0530

    Update index.tsx

--- src/app/movies/[id]/_components/Images/index.tsx
+++ src/app/movies/[id]/_components/Images/index.tsx
@@ -6,17 +6,27 @@ interface Props {
 }
 export default function Images({ id }: Props) {
   const { data, isLoading, error } = useImages(id);
-
+  if (error) throw error;
   return (
     <div className="mt-10">
-      <h4>Images</h4>
-      <div className=" overflow-scroll no-scrollbar">
-        <div className="flex gap-3 ">
-          {data?.backdrops?.map((img, index) => (
-            <MovieImage img={img} key={index} />
-          ))}
+      <h4 className="mb-5">Images</h4>
+      {isLoading ? (
+        <div className=" overflow-scroll no-scrollbar">
+          <div className="flex gap-3 ">
+            {[1, 1, 1, 1, 1, 1].map((_, index) => (
+              <div key={index} className="w-40 aspect-video" />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className=" overflow-scroll no-scrollbar">
+          <div className="flex gap-3 ">
+            {data?.backdrops?.map((img, index) => (
+              <MovieImage img={img} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
