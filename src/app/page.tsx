642be376824f0d56f6fd7b6cf1aca5c6f5b9ea97
sanadh <suriyaakku@gmail.com>
2024-10-19 00:42:33 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import PageLayout from "@/components/PageLayout";
 import NowPlaying from "./_components/now-palying";
 import Upcoming from "./_components/upcoming";
-import { auth } from "@/auth";
+
 export default async function Home() {
-  const session = await auth();
-  console.log(session?.user);
   return (
     <PageLayout className="">
       <div className="">
