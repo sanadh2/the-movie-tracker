9f2cb07ab79d2259cb2c644eb195fd96c02b1055
sanadh <suriyaakku@gmail.com>
2025-02-24 13:01:33 +0530

    added revalidating api's...

--- src/app/_components/movie-list.tsx
+++ src/app/_components/movie-list.tsx
@@ -15,7 +15,7 @@ const MovieList = ({ movies }: Props) => {
         <SuperLink
           href={
             env.NEXT_PUBLIC_API_BASE_URL +
-            "/movies/" +
+            "movies/" +
             generateSlug(movie.original_title, movie.id)
           }
           className="relative border border-secondary min-w-20 md:min-w-32 aspect-[9/14]"
