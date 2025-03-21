7f1e0df706628f42c34ac54afec5a73fae1d2479
sanadh <suriyaakku@gmail.com>
2025-03-22 00:37:08 +0530

    unwanted padding removed

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -8,10 +8,7 @@ interface Props extends PropsWithChildren {
 export default function PageLayout({ className, children }: Props) {
   return (
     <div
-      className={cn(
-        "p-1 md:p-3 lg:p-5 xl:p-7 2xl:p-14 container mx-auto",
-        className
-      )}
+      className={cn("p-3 md:p-5 xl:p-7 2xl:p-14 container mx-auto", className)}
     >
       {children}
     </div>
