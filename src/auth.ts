851f0ceaa4358f985bbd167925312692486e36fd
sanadh <suriyaakku@gmail.com>
2025-03-22 23:50:21 +0530

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,8 @@ import { getUserByEmail } from "./db/services/users";
 import bcrypt from "bcryptjs";
 import type { Provider } from "next-auth/providers";
 import { db } from "./db";
+import { accounts, users } from "@/db/schema/user";
+
 class InvalidLoginError extends CredentialsSignin {
   code = "Invalid identifier or password";
 }
@@ -53,6 +55,43 @@ export const authConfig: NextAuthConfig = {
         session.user.id = token.id as string;
       }
       return session;
+    },
+    async signIn(data) {
+      if (
+        data.user.email &&
+        data.account &&
+        data.account.provider !== "credentials"
+      ) {
+        const existingUser = await getUserByEmail(data.user.email);
+
+        if (existingUser) {
+          await db
+            .insert(accounts)
+            .values({
+              userId: existingUser.id,
+              ...data.account,
+              type: data.account.type as any,
+              provider: data.account.provider,
+              providerAccountId: data.account.providerAccountId,
+            })
+            .onConflictDoUpdate({
+              target: [accounts.provider, accounts.providerAccountId],
+              set: { userId: existingUser.id },
+            });
+        } else {
+          await db
+            .insert(users)
+            .values({
+              name: data.user.name!,
+              email: data.user.email!,
+              emailVerified: new Date(),
+              isEmailVerified: true,
+              image: data.user.image,
+            })
+            .returning();
+        }
+      }
+      return true;
     },
   },
   pages: {
